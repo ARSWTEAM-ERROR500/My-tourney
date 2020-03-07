efde0a32c35380b26c8c023a3d9ca11e3e8e8d82
Silenrate <daniwalteros596@gmail.com>
2020-03-07 16:09:48 -0500

    Creacion Estrucutura Front Basica

--- src/main/java/arsw/mytourney/controllers/MyTourneyAPIController.java
+++ src/main/java/arsw/mytourney/controllers/MyTourneyAPIController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/")
+@RequestMapping(value = "/a")
 public class MyTourneyAPIController {
 
     @RequestMapping(method = RequestMethod.GET)
